075f951d0af1573808353ffcc65d8dd9939869df
zen-young <renzcruz50@gmail.com>
2024-04-07 00:51:34 +0800

    implemented orbital controls

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xadd8e6);
@@ -12,6 +13,7 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -25,6 +27,7 @@ function animate() {
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  controls.update();
 
   renderer.render(scene, camera);
 }
